5b45151356b8db6c9888a026ef205169db11b291
Eduardo Díaz <iamedu@gmail.com>
2012-05-17 08:30:19 -0500

    Spring mvc almost ready

---
+++ src/main/resources/public/scripts/app/package/springmvc.js
@@ -0,0 +1,24 @@
+/*global require: false, provide: false */
+/*jslint nomen: true*/
+(function (context) {
+	"use strict";
+	
+	var _        = require('underscore'),
+	    $script  = require('scriptjs'),
+	    jQuery,
+	    install;
+	
+	$script('app/package/package', 'package');
+	
+	$script.ready(['jquery', 'bootstrap', 'package'], function () {
+		jQuery  = require('jquery');
+		
+		jQuery("#installSpringMvc").click(function () {
+			install = require('PackageInstall');
+			install('spring-mvc');
+		});
+		
+		
+	});
+	
+}());
