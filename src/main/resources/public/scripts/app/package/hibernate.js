468ad8788666d80cba8885a7ab1f6a4435644132
Eduardo Díaz <iamedu@gmail.com>
2012-05-21 12:28:41 -0500

    Adding hibernate package

---
+++ src/main/resources/public/scripts/app/package/hibernate.js
@@ -0,0 +1,28 @@
+/*global require: false, provide: false */
+/*jslint nomen: true*/
+(function (context) {
+	"use strict";
+	
+	var _        = require('underscore'),
+	    $script  = require('scriptjs'),
+	    jQuery,
+	    install;
+	
+	$script('app/package/package', 'package');
+	
+	$script.ready(['jquery', 'bootstrap', 'package'], function () {
+		console.log("Going"); 	
+		jQuery  = require('jquery');
+		
+		jQuery("#installHibernate").click(function () {
+			install = require('PackageInstall');
+			jQuery('#installHibernate').button('loading');
+			install('hibernate', function () {
+				jQuery('#installHibernate').button('reset');
+			});
+			
+		});
+		
+	});
+	
+}());
