57783745f05328165bda9be3b833b9196c0751ce
Eduardo Díaz <iamedu@gmail.com>
2012-07-07 07:58:12 -0500

    Adding license

--- src/main/java/com/justcloud/osgifier/SpringContextHolder.java
+++ src/main/java/com/justcloud/osgifier/SpringContextHolder.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2012 Just-Cloud
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.justcloud.osgifier;
 
 import java.util.ArrayList;
@@ -18,12 +33,14 @@ public class SpringContextHolder {
 
 	private static SpringContextHolder instance;
 	private Map<String, OsgiBundleXmlApplicationContext> contexts;
+	@SuppressWarnings("rawtypes")
 	private List<ServiceRegistration> regs;
 
 	static {
 		instance = new SpringContextHolder();
 	}
 
+	@SuppressWarnings("rawtypes")
 	private SpringContextHolder() {
 		contexts = new ConcurrentHashMap<String, OsgiBundleXmlApplicationContext>();
 		regs = new ArrayList<ServiceRegistration>();
@@ -33,6 +50,7 @@ public class SpringContextHolder {
 		return contexts.get(name);
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void registerContext(String name,
 			OsgiBundleXmlApplicationContext context) {
 		contexts.put(name, context);
@@ -46,6 +64,7 @@ public class SpringContextHolder {
 				context, properties));
 	}
 
+	@SuppressWarnings("rawtypes")
 	public void unregisterContext(String name) {
 		OsgiBundleXmlApplicationContext context = contexts.get(name);
 		contexts.remove(name);
